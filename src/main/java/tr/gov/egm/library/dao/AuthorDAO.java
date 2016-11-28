3eae5bdc0846601e1031b5ab904224855559feae
abbak <erkinpehlivan@gmail.com>
2016-11-28 10:38:00 +0200

    @repository eklendi

--- src/main/java/tr/gov/egm/library/dao/AuthorDAO.java
+++ src/main/java/tr/gov/egm/library/dao/AuthorDAO.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import tr.gov.egm.library.entities.Author;
 
+@Repository
 public interface AuthorDAO extends CrudRepository<Author, Integer> {
 
 	@Query("from Author where name like '%:adi%'")
